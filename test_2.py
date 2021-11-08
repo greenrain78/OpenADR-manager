ed0d2170fce81275198e7bc128f3595bc98f11d9
ehdtjs2651 <81279713+ehdtjs2651@users.noreply.github.com>
2021-11-08 17:52:37 +0900

    슬랙봇테스트test_2

---
+++ test_2.py
@@ -0,0 +1,15 @@
+import json
+import requests
+from pandas.io.json import json_normalize
+
+slack_token = 'xoxb-2637151350448-2663729429670-CXuyzwkDgjV0uXEE9zRfzeSl'
+# 파라미터
+data = {'Content-Type': 'application/x-www-form-urlencoded',
+        'token': slack_token,
+        'channel': 'test',
+        'text': 'testtttt',
+        }
+
+# 메시지 등록 API 메소드: chat.postMessage
+URL = "https://slack.com/api/chat.postMessage"
+res = requests.post(URL, data=data)
